2260bc09350bb0953938a93abc2c2d216f160a46
Petrara <31998191+Petrara@users.noreply.github.com>
2020-12-01 17:24:06 +0100

    Hello code added

--- src/main/java/Ruap.java
+++ src/main/java/Ruap.java
@@ -3,6 +3,10 @@ import static java.lang.System.out;
 public class Ruap {
 
     public static void main(String[] args) {
+<<<<<<< HEAD
         System.out.println("Pozdrav iz aplikacije od Petra Radocaj. Bez papirnatog indeksa :)");
+=======
+        System.out.println("Pozdrav iz aplikacije od Petra Radocaj.");
+>>>>>>> c551e66... Hello code added
     }
 }
