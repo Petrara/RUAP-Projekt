c551e661216c24709281b665b3b1cd4e30aab4f7
Petrara <31998191+Petrara@users.noreply.github.com>
2020-12-01 17:24:06 +0100

    Hello code added

--- src/main/java/Ruap.java
+++ src/main/java/Ruap.java
@@ -3,6 +3,6 @@ import static java.lang.System.out;
 public class Ruap {
 
     public static void main(String[] args) {
-        
+        System.out.println("Pozdrav iz aplikacije od Petra Radocaj.");
     }
 }
